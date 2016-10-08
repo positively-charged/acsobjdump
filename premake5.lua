be61296dd48e500f9eb78031e802ec5c90724506
Positron <positron@rogers.com>
2016-10-08 11:15:06 -0400

    Add source file and premake5 file

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+solution 'acsobjdump'
+   configurations 'release'
+   language 'C'
+
+   buildoptions {
+      '-Wall',
+      '-Werror',
+      '-Wno-error=switch',
+      '-Wno-unused',
+      '-std=c99',
+      '-pedantic',
+   }
+
+   flags {
+      'Symbols',
+   }
+
+   project 'acsobjdump'
+      location 'build'
+      kind 'ConsoleApp'
+
+      files {
+         '*.c',
+      }
